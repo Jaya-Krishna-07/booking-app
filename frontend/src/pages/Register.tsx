9a7a43a1f872459f70266975408dde2d4ab9ee33
Jaya Krishna Mannem <jayamannem07@gmail.com>
2024-12-20 18:06:55 +0530

    Frontend: edited the Register.tsx file that contains the register page, now it displays errors like 'this field is required' or 'password do not match' below the input field.

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -10,7 +10,7 @@ type RegisterFormData = {
 
 const Register = () => {
 
-  const { register, watch, handleSubmit } = useForm<RegisterFormData>();
+  const { register, watch, handleSubmit, formState: { errors } } = useForm<RegisterFormData>();
 
   const onSubmit = handleSubmit((data) => {
     console.log(data);
@@ -23,19 +23,31 @@ const Register = () => {
         <label className="text-gray-700 text-sm font-bold flex-1">
           First Name
           <input className="border rounded w-full py-1 px-2 font-normal" {...register("firstName", { required: "This field is required" })} />
+          {errors.firstName && (
+            <span className="text-red-500">{errors.firstName.message}</span>
+          )}
         </label>
         <label className="text-gray-700 text-sm font-bold flex-1">
           Last Name
           <input className="border rounded w-full py-1 px-2 font-normal" {...register("lastName", { required: "This field is required" })} />
+          {errors.lastName && (
+            <span className="text-red-500">{errors.lastName.message}</span>
+          )}
         </label>
       </div>
       <label className="text-gray-700 text-sm font-bold flex-1">
         Email
         <input type="email" className="border rounded w-full py-1 px-2 font-normal" {...register("email", { required: "This field is required" })} />
+        {errors.email && (
+          <span className="text-red-500">{errors.email.message}</span>
+        )}
       </label>
       <label className="text-gray-700 text-sm font-bold flex-1">
         Password
         <input type="password" className="border rounded w-full py-1 px-2 font-normal" {...register("password", { required: "This field is required", minLength: { value: 6, message: "Password must be at least 6 characters." } })} />
+        {errors.password && (
+          <span className="text-red-500">{errors.password.message}</span>
+        )}
       </label>
       <label className="text-gray-700 text-sm font-bold flex-1">
         Confirm Password
@@ -46,6 +58,9 @@ const Register = () => {
               else if (watch("password") !== val) return "Your passwords do not match."
             }
           })} />
+        {errors.confirmPassword && (
+          <span className="text-red-500">{errors.confirmPassword.message}</span>
+        )}
       </label>
       <span >
         <button type="submit" className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl">Create Account</button>
